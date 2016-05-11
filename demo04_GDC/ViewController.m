2e65bc9f4d7915ac3a8470e18c164245ea1ea406
LuoShimei <Luo_Shimei@163.com>
2016-05-11 16:13:41 +0800

    GCD项目的创建

---
+++ demo04_GDC/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  demo04_GDC
+//
+//  Created by LuoShimei on 16/5/11.
+//  Copyright © 2016年 罗仕镁. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+@end
---
+++ demo04_GDCTests/demo04_GDCTests.m
@@ -0,0 +1,39 @@
+//
+//  demo04_GDCTests.m
+//  demo04_GDCTests
+//
+//  Created by LuoShimei on 16/5/11.
+//  Copyright © 2016年 罗仕镁. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface demo04_GDCTests : XCTestCase
+
+@end
+
+@implementation demo04_GDCTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
